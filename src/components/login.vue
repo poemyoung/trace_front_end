f88a20a9dab7cc76b960e268588bd4ea1cef6bf0
徐泽朋 <2427389569@qq.com>
2021-03-15 18:27:50 +0800

    组件中绑定函数

--- src/components/login.vue
+++ src/components/login.vue
@@ -3,14 +3,24 @@
     <h2>管理员登录</h2>
     <el-input v-model="adm" placeholder="请输入账号"></el-input>
     <el-input v-model="pwd" show-password placeholder="请输入密码"></el-input>
+    <el-button @click="login">登录</el-button>
   </div>
 </template>
 <script>
 export default {
   name: "login",
-  props:['adm','pwd']
+  data() {
+    return {
+      adm : '',
+      pwd : ''
+    }
+  },
+  methods:{
+    login() {
+      // this.adm 可以拿到数据
+    }
+  }
 };
-
 </script>
 <style scoped>
 
