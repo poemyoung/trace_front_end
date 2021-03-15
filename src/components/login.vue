c08673fa0af8d3c8432bc1bd912b0f3c2bdcde50
徐泽朋 <2427389569@qq.com>
2021-03-15 12:42:36 +0800

    替换首页

---
+++ src/components/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="login">
+    <h2>管理员登录</h2>
+    <el-input v-model="adm" placeholder="请输入账号"></el-input>
+    <el-input v-model="pwd" show-password placeholder="请输入密码"></el-input>
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  props:['adm','pwd']
+};
+
+</script>
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import './plugins/element.js'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
+import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
