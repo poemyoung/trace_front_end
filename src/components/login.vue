cef2b0542abccdf80f67409915728177677322fc
徐泽朋 <2427389569@qq.com>
2021-03-16 16:54:06 +0800

    新建menu组件

--- src/components/login.vue
+++ src/components/login.vue
@@ -3,42 +3,54 @@
     <h2>管理员登录</h2>
     <el-input v-model="adm" placeholder="请输入账号"></el-input>
     <el-input v-model="pwd" show-password placeholder="请输入密码"></el-input>
-    <el-button @click="login">登录</el-button>
+    <el-button @click="login" type="primary">登录</el-button>
   </div>
 </template>
 <script>
-import Requests from '../requests'
+import Requests from "../requests";
 export default {
   name: "login",
   data() {
     return {
-      adm : '',
-      pwd : ''
-    }
+      adm: "",
+      pwd: "",
+    };
   },
-  methods:{
+  methods: {
     login() {
-      // this.adm 可以拿到数据
       // 向springboot后台发起登录请求
-      let response =  Requests.login(this.adm,this.pwd);
-      console.log(response);
-
-      if(this.adm == 'admin' && this.pwd == '123456'){
-        // 本地存储
-        localStorage.IsLogin = true;
-        localStorage.LoginTime = new Date();
-        // 跳转
-        window.location.href='/idx'
-      }else {
-        localStorage.IsLogin = false;
-        this.$alert('账号或密码错误!','错误',{
-          confirmButtonText: '确定'
-        })
-      }
-    }
-  }
+      Requests.login(this.adm, this.pwd)
+      .then((rep) => {
+        // 判断是否登录成功
+        if (rep == null || rep.code == 20002) {
+           localStorage.IsLogin = false;
+          this.$alert("账号或密码错误!", "错误", {
+            confirmButtonText: "确定",
+          });
+        } else {
+           // 本地存储
+          localStorage.IsLogin = true;
+          localStorage.LoginTime = new Date();
+          // 跳转
+          window.location.href = "/idx";
+        }
+      })
+      .catch(res => console.log(res));
+    },
+  },
 };
 </script>
 <style scoped>
-
+#login {
+  text-align: center;
+  margin:8% 38% 0 38%;
+  padding: 3% 3% 3% 3%
+}
+.el-button {
+  width: 100%;
+  margin-top: 5%;
+}
+.el-input {
+  padding-top: 1%;
+}
 </style>
--- src/requests.js
+++ src/requests.js
@@ -1,18 +1,20 @@
-const baseUrl = "https://localhost:443/webapi";
+const baseUrl = "https://www.poemyoung.xyz/webapi";
 import Axios from 'axios'
 
 export default {
-    login(admin,pwd) {
-        Axios
-        .post(baseUrl + "/login",{
-            name : admin,
-            password : pwd
-        })
-        .then(response => {
-            
-            console.log(response);
+    login(admin, pwd) {
+        return new Promise(function(resolve,reject){
+            Axios
+            .post(baseUrl + "/login", {
+                name: admin,
+                password: pwd
+            })
+            .then(response => {
+                resolve(response.data);
+            })
+            .catch(response => {
+                reject(response);
+            })
         })
     }
 }
-
-
